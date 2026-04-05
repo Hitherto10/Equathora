374fed77aa94d96b37e43f43032ad3e974e2874d
Zaim Halili <halilizaim@gmail.com>
2026-04-05 16:29:04 +0200

    feat: Update link destination from /learn to /dashboard in AboutHeroSection and HeroSection components

--- equathora/src/components/About/sections/AboutHeroSection.jsx
+++ equathora/src/components/About/sections/AboutHeroSection.jsx
@@ -73,7 +73,7 @@ const AboutHeroSection = () => {
                         className="flex flex-wrap justify-center items-center gap-3 sm:gap-4 pt-1"
                     >
                         <Link
-                            to="/learn"
+                            to="/dashboard"
                             className="group flex items-center gap-2 rounded-full !bg-[linear-gradient(360deg,var(--accent-color),var(--dark-accent-color))] px-5 sm:px-6 md:px-8 py-2.5 sm:py-3 text-sm sm:text-base text-center !text-white font-semibold transition-all ease-in hover:!bg-[linear-gradient(360deg,var(--dark-accent-color),var(--dark-accent-color))] shadow-lg shadow-[var(--raisin-black)]/30 active:translate-y-1"
                         >
                             Start practicing
